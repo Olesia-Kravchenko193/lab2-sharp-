c50bf3c1a232336a892de5d2e2ccf8647365c0c0
lesti <yaseloklass@gmail.com>
2020-03-21 23:59:35 +0300

    Добавьте файлы проекта.

---
+++ lab2(sharp)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_sharp_
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+           
+            Random random = new Random();
+            triangle.a = random.Next(1, 10);
+            Console.WriteLine("a = " + triangle.a);
+            triangle.b = random.Next(1, 10);
+            Console.WriteLine("b = " + triangle.b);
+            triangle.c = random.Next(1, 10);
+            Console.WriteLine("c = " + triangle.c);
+
+            if (triangle.isExists())
+            {
+                Console.WriteLine("Треугольник существует");
+                Console.WriteLine("Угол А = " + triangle.getAngle1());
+                Console.WriteLine("Угол В = " + triangle.getAngle2());
+                Console.WriteLine("Угол С = " + triangle.getAngle3());
+
+                Console.WriteLine("P = " + triangle.getPerimeter());
+                Console.WriteLine("S = " + triangle.getArea());
+            }
+            else
+                Console.WriteLine("Треугольник НЕ существует");
+        }
+    }
+}
---
+++ lab2(sharp)/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_sharp_
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;        
+        public bool isExists()
+        {
+            if (a + b > c && a + c > b && b + c > a)
+                return true;
+            else
+                return false;
+        }
+
+        public double getAngle1()
+        {
+            return Math.Acos((a * a + c * c - b * b) / (2 * a * c))*180/Math.PI;              
+        }
+        public double getAngle2()
+        {
+            return Math.Acos((a * a + b * b - c * c) / (2 * a * b )) * 180 / Math.PI;
+        }
+        public double getAngle3()
+        {
+            return Math.Acos((b * b + c * c - a * a) / (2 * c * b )) * 180 / Math.PI;
+        }
+        public double getPerimeter()
+        {          
+          return (a + b + c);           
+        }
+
+        public double getArea()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
